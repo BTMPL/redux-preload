81791a0afc9a57d2c7f258984eee1342ca551781
Bartosz Szczecinski <btm@anfo.pl>
2017-04-16 16:32:13 +0200

    Changed src to es6; support for adding untranspiled code to projects

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-  entry: path.join(__dirname, "src", "index.js"),
+  entry: path.join(__dirname, "es6", "index.js"),
 
   output: {
     filename: "index.js",
